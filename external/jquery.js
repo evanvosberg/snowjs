8813cfdbfd4faee7cc3ed95fa368bf7bb2a38621
Evan Vosberg <evan.vosberg@inext.me>
2013-02-04 10:07:58 +0100

    Update jQuery to version 1.9.0

--- external/jquery.js
+++ external/jquery.js
@@ -1,3 +1,27 @@
-var version = '1.8.3';
+var version = '1.9.0',
+	migrate = '1.1.0';
 
 snow.add('jquery.js', snow.fetch('http://code.jquery.com/jquery-' + version + '.js'));
+
+snow.add('jquery-sdk.js', [
+	snow.fetch('http://code.jquery.com/jquery-' + version + '.js')
+		.toString(),
+	snow.fetch(__dirname + '/jquery/jquery-sdk.js')
+		.toString()
+]);
+
+snow.add('jquery-migrate.js', [
+	snow.fetch('http://code.jquery.com/jquery-' + version + '.js')
+		.toString(),
+	snow.fetch('http://code.jquery.com/jquery-migrate-' + migrate + '.js')
+		.toString()
+]);
+
+snow.add('jquery-migrate-sdk.js', [
+	snow.fetch('http://code.jquery.com/jquery-' + version + '.js')
+		.toString(),
+	snow.fetch('http://code.jquery.com/jquery-migrate-' + migrate + '.js')
+		.toString(),
+	snow.fetch(__dirname + '/jquery/jquery-sdk.js')
+		.toString()
+]);
