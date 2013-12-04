e11e9eb4076bf359cca2cff28ea7c40ec69769ae
evanvosberg <evanvosberg@inext.me>
2013-12-04 15:06:50 +0100

    Update resource location of jQuery metadata plugin

--- external/jquery-metadata.js
+++ external/jquery-metadata.js
@@ -3,7 +3,7 @@ var version = 'master';
 snow.add('jquery/metadata.js', [
 	'define("jquery/metadata", ["jquery"], function (jQuery) {',
 	'',
-	snow.fetch('https://raw.github.com/jquery/jquery-metadata/' + version + '/jquery.metadata.js')
+	snow.fetch('https://raw.github.com/jquery-orphans/jquery-metadata/' + version + '/jquery.metadata.js')
 		.toString(),
 	'',
 	'	return jQuery;',
