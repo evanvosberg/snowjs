82c5449b29e048ccbbc131d74159671dfd9eced9
evanvosberg <evanvosberg@inext.me>
2013-12-04 15:06:38 +0100

    Update resource location of jQuery datalink plugin

--- external/jquery-datalink.js
+++ external/jquery-datalink.js
@@ -3,7 +3,7 @@ var version = 'master';
 snow.add('jquery/fn/datalink.js', [
 	'define("jquery/fn/datalink", ["jquery"], function (jQuery) {',
 	'',
-	snow.fetch('https://raw.github.com/jquery/jquery-datalink/' + version + '/jquery.datalink.js')
+	snow.fetch('https://raw.github.com/jquery-orphans/jquery-datalink/' + version + '/jquery.datalink.js')
 		.toString(),
 	'',
 	'	return jQuery;',
